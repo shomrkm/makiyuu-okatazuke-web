e2615edd1736463543c22250f7ec881d7a314fa4
shomrkm <shochan1985@gmail.com>
2024-01-27 16:39:07 +0900

    refact: LinBox をReactコンポーネント化

---
+++ src/components/elements/LinkBox.tsx
@@ -0,0 +1,12 @@
+import React, { type ReactNode } from 'react'
+
+type Props = {
+  href: string;
+  children: ReactNode;
+}
+
+export const LinkBox = ({href, children}: Props) => (
+  <a href={href} className="flex justify-center items-center bg-accent text-slate-100 w-full lg:h-20 min-w-40 min-h-10">
+    {children}
+  </a>
+)
