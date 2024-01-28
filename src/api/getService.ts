e719d075a4b9940180f931c332dd90d2bf822f42
shomrkm <shochan1985@gmail.com>
2024-01-28 15:34:01 +0900

    feat: サービスページの暫定実装

---
+++ src/api/getService.ts
@@ -0,0 +1,11 @@
+import type { Service } from "../types/service";
+
+export const getService = async (): Promise<Service> => {
+  const response = await fetch("https://makiyuu-okatazuke.cdn.newt.so/v1/makiyuu-okatazuke/services", {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.NEWT_TOKEN}`,
+      },
+    });
+  const services = await response.json()
+  return services.items[0];
+}
---
+++ src/types/service.ts
@@ -0,0 +1,12 @@
+
+export type Service = {
+  _id: string;
+  title: string;
+  description: string;
+  body: string;
+  _sys: {
+    createdAt: string;
+    updatedAt: string;
+    customOrder: 3,
+  }
+}
