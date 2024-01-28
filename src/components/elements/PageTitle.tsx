c94e62e931711db2f8c2d24578533a70d086e3ea
shomrkm <shochan1985@gmail.com>
2024-01-28 13:59:57 +0900

    feat: トップページを実装

--- src/components/elements/PageTitle.tsx
+++ src/components/elements/PageTitle.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const PageTitle = ({children}: Props) => {
   return (
     <div className='flex justify-center items-center w-auto'>
-      <h1 className='bg-accent text-slate-200 sm:text-3xl text-xl px-10 py-2'>{children}</h1>
+      <h1 className='bg-accent text-slate-200 md:text-3xl text-base md:px-10 px-2 py-2'>{children}</h1>
     </div>
   )
 }
