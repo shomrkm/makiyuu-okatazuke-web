3bca7e44f6a23385a997bd1b7d77f2009e5d0b59
shomrkm <shochan1985@gmail.com>
2024-02-03 11:53:17 +0900

    feat: フッターを追加

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="sticky flex justify-end items-center bg-primary text-white w-full h-14 mt-10">
+      <p className='mr-4 text-sm'>Copyright © 2024 Makiyuu Okatazuke. All Rights Reserved.</p>
+    </footer>
+  )
+}
