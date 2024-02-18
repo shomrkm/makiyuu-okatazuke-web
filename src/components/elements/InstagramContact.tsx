db1a003f875cb4617d4dfe7adabbcd00bbf90097
shomrkm <shochan1985@gmail.com>
2024-02-18 11:18:25 +0900

    feat: Instagram アカウントをクリックすると、Instagramへ遷移するように変更

--- src/components/elements/InstagramContact.tsx
+++ src/components/elements/InstagramContact.tsx
@@ -2,9 +2,11 @@ import { ImInstagram } from "react-icons/im";
 
 export const InstagramContact = () => {
   return (
-    <div className="flex items-center gap-2">
-      <ImInstagram />
-      <span>@makiyuu_okatazuke</span>
-    </div>
+    <a href="https://www.instagram.com/makiyuu_okatazuke">
+      <div className="flex items-center gap-2">
+        <ImInstagram />
+        <span>@makiyuu_okatazuke</span>
+      </div>
+    </a>
   )
 }
