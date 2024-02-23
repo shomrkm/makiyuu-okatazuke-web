a7691ab5c4c1ee91e6b5b9fb2b35d2bda55980ee
shomrkm <shochan1985@gmail.com>
2024-02-23 11:04:05 +0900

    feat: ページタイトルの大きさを調整

--- src/components/elements/PageTitle.tsx
+++ src/components/elements/PageTitle.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const PageTitle = ({children}: Props) => {
   return (
     <div className='flex justify-center items-center w-auto'>
-      <h1 className='text-accent sm:text-4xl text-2xl md:px-10 px-2 py-2 border-b-4 border-accent'>{children}</h1>
+      <h1 className='text-accent sm:text-4xl text-2xl px-4 py-2 border-b-4 border-accent'>{children}</h1>
     </div>
   )
 }
