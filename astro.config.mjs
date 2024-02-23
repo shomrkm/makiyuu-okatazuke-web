5843d6c8b1ab407c23f47307eb95108cb267ab8b
shomrkm <shochan1985@gmail.com>
2024-02-23 14:15:26 +0900

    chore: add sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://makiyuu-okatazuke.netlify.app",
+  integrations: [tailwind(), react(), sitemap()],
   image: {
-    domains: ["astro.build"],
+    domains: ["astro.build"]
   }
 });
