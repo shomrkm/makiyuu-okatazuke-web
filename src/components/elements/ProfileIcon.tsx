7ad2c1bdb26a261b359e15a73d34be6fad0554fd
shomrkm <shochan1985@gmail.com>
2024-02-23 10:05:52 +0900

    refact: Refactoring ProfileIcon

--- src/components/elements/ProfileIcon.tsx
+++ src/components/elements/ProfileIcon.tsx
@@ -1,15 +1,15 @@
 import clsx from "clsx";
 import ProfileImage from '../../pages/images/makiyuu-profile.jpg'
 
-type Props = {
-  size?: "sm" | "md" | "lg";
-  className?: string;
-}
-
 const sizes = {
   sm: 'w-10 h-10',
   md: 'w-20 h-20',
   lg: 'w-32 h-32',
+}
+
+type Props = {
+  size?: keyof typeof sizes
+  className?: string;
 }
 
 export const ProfileIcon = ({ size = 'sm', className }: Props) => {
