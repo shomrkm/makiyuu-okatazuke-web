798bb5441ed1e00e47f1bbe0c3650064be9dfb94
shomrkm <shochan1985@gmail.com>
2024-02-23 15:35:14 +0900

    chore: Enable SSR.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +10,7 @@ export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
   image: {
     domains: ["astro.build"]
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
